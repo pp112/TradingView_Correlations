a4b9695c42efbdaf900abb611edeb842468eb1cc
Artem Milaev <artemik.milaev.2004@mail.ru>
2026-03-14 00:50:29 +0700

    Обновление функции add_tickers_to_list

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ class TradingViewParser:
     
     # Получить все тикеры в TradingView
     def get_all_tickers(self):
+
         url = "https://symbol-search.tradingview.com/symbol_search/v3/"
         headers = {
             "User-Agent": "Mozilla/5.0",
@@ -48,7 +49,6 @@ class TradingViewParser:
             "search_type": "crypto_swap",
             "start": 0
         }
-
         start = 0
 
         while True:
@@ -70,58 +70,75 @@ class TradingViewParser:
         
     # Добавить тикеры в список
     def add_tickers_to_list(self, tickers):
-        self.driver.click("button[data-name='watchlists-button']")
-        self.driver.get_element_with_exact_text("Очистить список").click()
-        self.driver.click("button[data-qa-id='yes-btn']")
-        
-        self.driver.click("button[data-name='add-symbol-button']")
-        for ticker in self.tickers_:
-            ticker.click()
 
-        for ticker in tickers:
-            self.driver.type("input[data-qa-id='symbol-search-input']", ticker)
-            self.driver.click(".listContainer-dlewR1s1 .descriptionCell-oRSs8UQo")
-            self.driver.click("button[aria-label='Очистить']")
-        
-        self.driver.click("button[data-qa-id='close']")
+        watchlist_get_url = "https://ru.tradingview.com/api/v1/symbols_list/colored/red?source=web"
+        watchlist_add_url = "https://ru.tradingview.com/api/v1/symbols_list/colored/red/append/?source=web"
+        watchlist_remove_url = "https://ru.tradingview.com/api/v1/symbols_list/colored/red/remove/?source=web"
+
+        headers = {
+            "Content-Type": "application/json",
+            "User-Agent": "Mozilla/5.0",
+            "Origin": "https://ru.tradingview.com",
+            "Referer": "https://ru.tradingview.com/chart/RRGuoDgP"
+        }
+
+        cookies = self.driver.get_cookies_dict()
+
+        symbols_data = [f"BYBIT:{tikcer}" for tikcer in tickers]
+
+        watchlist_get_response = requests.get(url=watchlist_get_url, headers=headers, cookies=cookies)
+        watchlist_get_response.raise_for_status()
+
+        watchlist = watchlist_get_response.json()["symbols"]
+        if watchlist:
+            watchlist_remove_response = requests.post(url=watchlist_remove_url, headers=headers, 
+                                                      cookies=cookies, json=watchlist)
+            watchlist_remove_response.raise_for_status()
+
+        watchlist_add_response = requests.post(url=watchlist_add_url, headers=headers, 
+                                               cookies=cookies, json=symbols_data)
+        watchlist_add_response.raise_for_status()
+
+        self.driver.reload()
 
     # Пройтись по списку добавленных тикеров и собрать корреляции
     def collect_correlations_from_watchlist(self):
         tickers_in_watchlist = self.driver.select_all(".listContainer-MgF6KBas .firstItem-RsFlttSS")
-        
-        self.driver.enable_human_mode()
+        ticker_coord = []
 
-        for ticker in tickers_in_watchlist:
-            ticker.click()
+        for el in tickers_in_watchlist:
+            rect = el.get_bounding_rect()
+            ticker_coord.append(rect["center"])
+
+        for x, y in ticker_coord:
+            self.driver.click_at_point(int(x), int(y))
             self.get_correlation()
-        
-        self.driver.disable_human_mode()
+        # TODO: Пролистывание списка
 
     # Получить корреляцию с графика
     def get_correlation(self):
         ticker = self.driver.select("[data-qa-id='details-element symbol']").text
         correlation = float(
-            self.driver.select(".sources-l31H9iuA .valueValue-l31H9iuA").text.replace(",", ".")
+            self.driver.select(".sources-l31H9iuA .valueValue-l31H9iuA").text.replace(",", ".").replace("−", "-")
         )
         self.tickers_correlations[ticker] = correlation
 
     # Проход по тикерам
     def process_tickers(self):
         batch_size = 30
-        self.add_tickers_to_list(self.tickers_)
-        # for i in range(0, len(self.all_tickers), batch_size):
+        for i in range(0, len(self.all_tickers), batch_size):
         
-        #     ticker_batch = self.all_tickers[i:i + batch_size]
+            ticker_batch = self.all_tickers[i:i + batch_size]
 
-        #     self.add_tickers_to_list(ticker_batch)
-        #     self.collect_correlations_from_watchlist()
+            self.add_tickers_to_list(ticker_batch)
+            self.collect_correlations_from_watchlist()
     
     # Собрать все корреляции
     def collect_all_correlations(self):
         self.open_tradingview()
         self.get_all_tickers()
-        # self.activate_corr_indicator()
-        # self.process_tickers()
+        self.activate_corr_indicator()
+        self.process_tickers()
 
         return self.tickers_correlations
 
