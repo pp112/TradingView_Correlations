528c4c81a5108ced39c76061706b7c11072b9564
Artem Milaev <artemik.milaev.2004@mail.ru>
2026-03-15 02:16:00 +0700

    Обновлена функция collect_correlations

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 import json
 
 
-class TradingViewParser:
+class TradingViewCorrs:
     def __init__(self, driver: Driver):
         self.driver = driver
         self.cookies = {}
@@ -59,51 +59,6 @@ class TradingViewParser:
         
         self.all_tickers = [item["s"] for item in data_response["data"]]
 
-    # Добавить тикеры в список
-    def add_tickers_to_list(self, tickers):
-
-        watchlist_get_url = "https://ru.tradingview.com/api/v1/symbols_list/colored/red?source=web"
-        watchlist_add_url = "https://ru.tradingview.com/api/v1/symbols_list/colored/red/append/?source=web"
-        watchlist_remove_url = "https://ru.tradingview.com/api/v1/symbols_list/colored/red/remove/?source=web"
-
-        headers = {
-            "Content-Type": "application/json",
-            "User-Agent": "Mozilla/5.0",
-            "Origin": "https://ru.tradingview.com",
-            "Referer": "https://ru.tradingview.com/chart/RRGuoDgP"
-        }
-
-        symbols_data = [f"BYBIT:{tikcer}" for tikcer in tickers]
-
-        watchlist_get_response = requests.get(url=watchlist_get_url, headers=headers, cookies=self.cookies)
-        watchlist_get_response.raise_for_status()
-
-        watchlist = watchlist_get_response.json()["symbols"]
-        if watchlist:
-            watchlist_remove_response = requests.post(url=watchlist_remove_url, headers=headers, 
-                                                      cookies=self.cookies, json=watchlist)
-            watchlist_remove_response.raise_for_status()
-
-        watchlist_add_response = requests.post(url=watchlist_add_url, headers=headers, 
-                                               cookies=self.cookies, json=symbols_data)
-        watchlist_add_response.raise_for_status()
-
-        self.driver.reload()
-
-    # Пройтись по списку добавленных тикеров и собрать корреляции
-    def collect_correlations_from_watchlist(self):
-        tickers_in_watchlist = self.driver.select_all(".listContainer-MgF6KBas .firstItem-RsFlttSS")
-        ticker_coord = []
-
-        for el in tickers_in_watchlist:
-            rect = el.get_bounding_rect()
-            ticker_coord.append(rect["center"])
-
-        for x, y in ticker_coord:
-            self.driver.click_at_point(int(x), int(y))
-            self.get_correlation()
-        # TODO: Пролистывание списка
-
     # Получить корреляцию с графика
     def get_correlation(self):
         ticker = self.driver.select("[data-qa-id='details-element symbol']").text
@@ -111,23 +66,39 @@ class TradingViewParser:
             self.driver.select(".sources-l31H9iuA .valueValue-l31H9iuA").text.replace(",", ".").replace("−", "-")
         )
         self.tickers_correlations[ticker] = correlation
-
-    # Проход по тикерам
-    def process_tickers(self):
-        batch_size = 30
-        for i in range(0, len(self.all_tickers), batch_size):
-        
-            ticker_batch = self.all_tickers[i:i + batch_size]
-
-            self.add_tickers_to_list(ticker_batch)
-            self.collect_correlations_from_watchlist()
     
+    # Пройтись по списку добавленных тикеров и собрать корреляции
+    def collect_correlations(self):
+        self.driver.click("button[data-name='screener-dialog-button']")
+        
+        template_name_screener = self.driver.select("div[data-qa-id='screen-title']")
+        if template_name_screener.text != "Фьючерсы Bybit":
+            template_name_screener.click()
+            self.driver.get_element_with_exact_text("Фьючерсы Bybit").click()
+
+        total_tickers = len(self.all_tickers)
+        tickers = self.driver.select_all("tbody tr")
+        i = 0
+
+        while i < len(tickers):
+            ticker = tickers[i]
+            ticker.click()
+            self.get_correlation()
+
+            i += 1
+
+            if i >= len(tickers) - 5:
+                tickers = self.driver.select_all("tbody tr")
+            
+            if len(self.tickers_correlations) == total_tickers:
+                break
+
     # Собрать все корреляции
-    def collect_all_correlations(self):
+    def get_correlations(self):
         self.open_tradingview()
         self.get_all_tickers()
         self.activate_corr_indicator()
-        self.process_tickers()
+        self.collect_correlations()
 
         return self.tickers_correlations
 
@@ -138,8 +109,8 @@ class TradingViewParser:
     wait_for_complete_page_load=True
 )
 def main(driver: Driver, data):
-    tv = TradingViewParser(driver)
-    tickers_correlations = tv.collect_all_correlations()
+    tv = TradingViewCorrs(driver)
+    tickers_correlations = tv.get_correlations()
     print(tickers_correlations)
     
 main()
